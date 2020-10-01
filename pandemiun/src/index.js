84bc70f76e7d2284256ebb5fd290573caf7834cb
Daniel Ruiz Medina <drm19933@gmail.com>
2020-10-01 20:07:23 +0200

    librerias, rutas y configuraciones #8

--- pandemiun/src/index.js
+++ pandemiun/src/index.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 
 //settings
-app.set('port', process.env.PORT || 3000);//process.env.PORT PARA LUEGO EMITIR EN EL PUERTO DE LA NUBE QUE NOS DEJEN USAR
-app.set('json spaces', 2);
+app.set( 'port', process.env.PORT || 3000 );//process.env.PORT PARA LUEGO EMITIR EN EL PUERTO DE LA NUBE QUE NOS DEJEN USAR
+app.set( 'json spaces', 2 );
 
-//middleware 
-app.use(morgan('dev'));//me permite ver por consola lo que llega al servidor
-
-app.use(express.urlencoded({extended: false}));//nuestro servidor recoja-entienda los datos enviados por formularios
-app.use(express.json());
+app.use( express.urlencoded( {extended: false} ) );//nuestro servidor recoja-entienda los datos enviados por formularios
+app.use( express.json() );//permite al servidor recibir texto formato json (nuestro servidor va a enviar/ recibir json)
 
 //ROUTES
-app.use('/api',require('./routes/index.js'));//utliza las rutas pero todas empiezan por 'api'
+app.use( '/api/pandemiun' , require('./routes/index.js') );
 
-//Empezando el servidor
-app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`);
+//Lanzar servicio en el puerto indicado
+app.listen( app.get( 'port' ) , () => {
+    console.log( `Server on port ${ app.get( 'port' ) }` );
 });
--- pandemiun/src/routes/index.js
+++ pandemiun/src/routes/index.js
@@ -1,21 +1,20 @@
 const { Router } = require('express');//llama al metodo de express que me permite ejecutar muchas rutas
 const router = Router();
 
-const fetch = require('node-fetch');//para pedir recursos a otra api
+router.get('/:provincia/:desde/:hasta',(req,res) => {//para no tener que instanciar app
+    res.send("Correcto get /api/pandemiun/provincia/fecha_desde/fecha_hasta");
+});
 
-router.get('/pandemiun/:provincia/:rango', async (req,res) => {//para no tener que instanciar app
-    const { provincia, rango } = req.params;
+router.post('/:provincia/:desde/:hasta',(req,res) => {//para no tener que instanciar app
+    res.send("Correcto post /api/pandemiun/provincia/fecha_desde/fecha_hasta");
+});
 
-    if( provincia && rango ){
+router.delete('/:provincia/:desde/:hasta',(req,res) => {//para no tener que instanciar app
+    res.send("Correcto delete /api/pandemiun/provincia/fecha_desde/fecha_hasta");
+});
 
-        const response = await fetch('https://api.covid19tracking.narrativa.com/api/country/spain/region/andalucia?date_from=2020-09-30&date_to=2020-10-01');
-
-        const data = await response.json();
-
-        res.json(data);//peticion al micro servicio
-    }else{
-        res.send("Url mal introducida");
-    }
+router.put('/:provincia/:desde/:hasta',(req,res) => {//para no tener que instanciar app
+    res.send("Correcto put /api/pandemiun/provincia/fecha_desde/fecha_hasta");
 });
 
 module.exports = router;
