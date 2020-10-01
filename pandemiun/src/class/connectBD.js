459b4f35fbf6b23e2c32bace0454cdddbb77024a
Daniel Ruiz Medina <drm19933@gmail.com>
2020-10-01 21:36:16 +0200

    clase conectarBD close #11

---
+++ pandemiun/src/class/connectBD.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+class ConnectBD //clase que procesa la lógica de los datos
+{
+
+    constructor()
+    {
+
+        //mongoose.connect('mongodb:://localhost/pandemiun');
+
+    }
+
+    select (datos) {
+
+    }
+
+    update (datos) {
+
+    }
+
+    delete (datos) {
+
+    }
+
+    add (datos) {
+
+    }
+
+}
+
+module.exports = new ConnectBD();
