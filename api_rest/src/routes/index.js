9073a0701b284cfdfe6dc102d9e77a9e8cc50285
Daniel Ruiz Medina <drm19933@gmail.com>
2020-10-01 12:19:21 +0200

    Implementar API REST 2 #4

--- api_rest/src/routes/index.js
+++ api_rest/src/routes/index.js
@@ -1,11 +1,18 @@
 const { Router } = require('express');//llama al metodo de express que me permite ejecutar muchas rutas
 const router = Router();
 
-router.get('/pandemiun/:provincia/:rango',(req,res) => {//para no tener que instanciar app
+const fetch = require('node-fetch');//para pedir recursos a otra api
+
+router.get('/pandemiun/:provincia/:rango', async (req,res) => {//para no tener que instanciar app
     const { provincia, rango } = req.params;
 
     if( provincia && rango ){
-        res.send("bien");//peticion al micro servicio
+
+        const response = await fetch('https://api.covid19tracking.narrativa.com/api/country/spain/region/andalucia?date_from=2020-09-30&date_to=2020-10-01');
+
+        const data = await response.json();
+
+        res.json(data);//peticion al micro servicio
     }else{
         res.send("Url mal introducida");
     }
