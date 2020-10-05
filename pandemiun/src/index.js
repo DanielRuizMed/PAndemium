7a374ff9c504cd67438df1208f2e70991bf84886
Daniel Ruiz Medina <drm19933@gmail.com>
2020-10-05 11:07:12 +0200

    close #8

--- pandemiun/src/index.js
+++ pandemiun/src/index.js
@@ -6,7 +6,7 @@ app.set( 'port', process.env.PORT || 3000 );//process.env.PORT
 app.set( 'json spaces', 2 );
 
 app.use( express.urlencoded( {extended: false} ) );//nuestro servidor recoja-entienda los datos enviados por formularios
-app.use( express.json() );//permite al servidor recibir texto formato json (nuestro servidor va a enviar/ recibir json)
+app.use( express.json() );//permite al servidor recibir texto formato json
 
 //ROUTES
 app.use( '/api/pandemiun' , require('./routes/index.js') );
