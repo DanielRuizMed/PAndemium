30aff169d92848a6cc8a0e53f985e79284e8d012
Daniel Ruiz Medina <drm19933@gmail.com>
2020-10-14 17:34:38 +0200

    clase pandemiun metodos close #28

--- pandemiun/src/class/pandemiun.js
+++ pandemiun/src/class/pandemiun.js
@@ -3,23 +3,43 @@ const bd = require('./connectBD.js')
 class Pandemiun //clase que procesa la lógica de los datos
 {
 
-    constructor()
-    {
+    constructor() { 
 
+        console.log(this.updateDatos("","",""));
     }
 
-    getDatos (provincia,fecha,estado) {
+    getDatos (provincia,fecha) {
 
+        let resultado = "fecha mal o provincia vacia";
+        
+        if( /^\d{4}-\d{2}-\d{2}$/.test(fecha) && provincia )
+            resultado = bd.select(provincia,fecha);
+
+        return resultado;
     }
 
-    updateDatos (nick,provincia,fecha,estado) {
+    updateDatos (nick,provincia,estado) {
 
+        let resultado = "nick, provincia o estado sin rellenar";
+        
+        //*** TAREA: crear lista de provincias y estados fija */
+        if( nick && provincia && estado )
+            resultado = bd.update(nick,provincia,estado);
+
+        return resultado;
     }
 
-    addDatos (nick,provincia,fecha,estado) {
+    addDatos (nick,provincia,estado) {
 
+        let resultado = "nick, provincia o estado sin rellenar";
+        
+        //*** TAREA: crear lista de provincias y estados fija */
+        if( nick && provincia && estado )
+            resultado = bd.add(nick,provincia,estado);
+
+        return resultado;
     }
 
 }
 
-module.exports = Pandemiun; 
+module.exports = new Pandemiun(); 
