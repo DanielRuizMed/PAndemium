982c647cb174629ba02c40b3abb2eed24f46cd86
Daniel Ruiz Medina <drm19933@gmail.com>
2020-10-19 15:10:09 +0200

    método controlador de consultar datos #38, close #47

--- pandemiun/src/class/pandemiun.js
+++ pandemiun/src/class/pandemiun.js
@@ -1,16 +1,21 @@
-const bd = require('./connectBD.js')
+const li = require('./ListadoContagios.js')
 
 class Pandemiun //clase que procesa la lógica de los datos
 {
 
     constructor() { 
 
-
+        console.log(this.getDatos("Granada","2020-10-13"));
     }
 
     getDatos (provincia,fecha) {
 
+        let resultado = "fecha mal o provincia vacia";
         
+        if( /^\d{4}-\d{2}-\d{2}$/.test(fecha) && provincia )
+            resultado = li.select(provincia,fecha);
+
+        return resultado;
     }
 
     updateDatos (nick,provincia,estado) {
