8a6161b24d5b21973fc691277f6394f4fa653f91
Daniel Ruiz Medina <drm19933@gmail.com>
2020-10-19 18:11:32 +0200

    verificando excepciones de consulta de datos #53, close #55

--- pandemiun/test/testUpdate.js
+++ pandemiun/test/testUpdate.js
@@ -1,7 +1,5 @@
 //Requerimos paquetes
-let chai = require('chai');
 const expect = require('chai').expect;
-const should = chai.should();
 
 const pandemiun = require('../src/class/pandemiun')
 
@@ -9,26 +7,26 @@ const pandemiun = require('../src/class/pandemiun')
 describe('Capacidad de actualizar los datos de un usuarios', () => {
 
     //Test a funcion
-    context('2. metodo updateDatos(nick,provincia,estado) ', function() {
+    context('3. metodo updateDatos(nick,provincia,estado) ', function() {
 
-        it('2.1 actualizar valores existentes', function() {
+        it('3.1 actualizar valores existentes', function() {
             expect( pandemiun.updateDatos("Juan","Granada","contagiados") ).to.be.equal("actualización correcta");
         });
 
-        it('2.2 valores existentes, provincia no existente en base de datos', function() {
-            expect( pandemiun.updateDatos("Juan","Grana","contagiados") ).to.be.equal("la provincia no existe");
+        it('3.2 valores existentes, provincia no existente en base de datos', function() {
+            expect( pandemiun.updateDatos.bind("Juan","Grana","contagiados") ).to.throw("nick, provincia o estado sin rellenar");
         });
 
-        it('2.3 valores existentes, nick no existente en base de datos', function() {
-            expect( pandemiun.updateDatos("Ju","Granada","contagiados") ).to.be.equal("el nick no existe");
+        it('3.3 valores existentes, nick no existente en base de datos', function() {
+            expect( pandemiun.updateDatos.bind("Ju","Granada","contagiados") ).to.throw("nick, provincia o estado sin rellenar");
         });
 
-        it('2.4 valores vacios', function() {
-            expect( pandemiun.updateDatos("","","") ).to.be.equal("nick, provincia o estado sin rellenar");
+        it('3.4 valores vacios', function() {
+            expect( pandemiun.updateDatos.bind("","","") ).to.throw("nick, provincia o estado sin rellenar");
         });
 
-        it('2.4 solo 1 valor vacio', function() {
-            expect( pandemiun.updateDatos("","Granada","contagiados") ).to.be.equal("nick, provincia o estado sin rellenar");
+        it('3.4 solo 1 valor vacio', function() {
+            expect( pandemiun.updateDatos.bind("","Granada","contagiados") ).to.throw("nick, provincia o estado sin rellenar");
         });
 
     });
