f39fd23be5634f788a2381a1ee7ef7b3719c90dc
Daniel Ruiz Medina <drm19933@gmail.com>
2020-10-19 19:03:22 +0200

    subimos Gruntfile.js, archivo que gestiona grunt close #56

---
+++ pandemiun/Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+
+     // Add the grunt-mocha-test tasks.
+  grunt.loadNpmTasks('grunt-mocha-test');
+ 
+  grunt.initConfig({
+    // Configure a mochaTest task
+    mochaTest: {
+        src: ['test/**/*.js']
+    }
+    
+  });
+ 
+  grunt.registerTask('default', 'mochaTest');
+}
