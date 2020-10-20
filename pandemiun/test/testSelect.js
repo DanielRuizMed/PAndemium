07e34a4557e7d4e741b3cc943611ccf4b278eb7a
Daniel Ruiz Medina <drm19933@gmail.com>
2020-10-20 13:06:41 +0200

    reescribiendo los test con verificación de excepciones #41, close #54

--- pandemiun/test/testSelect.js
+++ pandemiun/test/testSelect.js
@@ -1,34 +1,54 @@
-//Requerimos paquetes
+let chai = require('chai');
 const expect = require('chai').expect;
 
 const pandemiun = require('../src/class/pandemiun')
 
+//usamos chaihttp para hacer peticiones a la API
+chai.use(require('chai-http'));
+const url= 'http://localhost:3000';
 
-describe('Capacidad de actualizar los datos de un usuarios', () => {
+describe('Consultar número de contagios, posibles o curados ', () => {
 
     //Test a funcion
-    context('3. metodo updateDatos(nick,provincia,estado) ', function() {
+    context('1. metodo getDatos(provincia,fecha) ', function() {
 
-        it('3.1 actualizar valores existentes', function() {
-            expect( pandemiun.updateDatos("Juan","Granada","contagiados") ).to.be.equal("actualización correcta");
+        it('1.1.1 con parametros existentes en base de datos provincia = Granada y fecha = 2020-10-13', function() {
+
+        expect( pandemiun.getDatos("Granada","2020-10-13") ).to.have.property('provincia').to.be.equal("Granada");
         });
 
-        it('3.2 valores existentes, provincia no existente en base de datos', function() {
-            expect( pandemiun.updateDatos.bind("Juan","Grana","contagiados") ).to.throw("nick, provincia o estado sin rellenar");
+        it('1.1.2 con provincia existente y fecha no', function() {
+
+        expect( pandemiun.getDatos.bind("Granada","2020-10-12") ).to.throw("fecha mal o provincia vacia");
         });
 
-        it('3.3 valores existentes, nick no existente en base de datos', function() {
-            expect( pandemiun.updateDatos.bind("Ju","Granada","contagiados") ).to.throw("nick, provincia o estado sin rellenar");
+        it('1.1.3 con provincia no existente y fecha si', function() {
+
+        expect( pandemiun.getDatos.bind("Gra","2020-10-13") ).to.throw("fecha mal o provincia vacia");
         });
 
-        it('3.4 valores vacios', function() {
-            expect( pandemiun.updateDatos.bind("","","") ).to.throw("nick, provincia o estado sin rellenar");
+        it('1.2.1 con provincia y fecha vacias', function() {
+
+        expect( pandemiun.getDatos.bind("","") ).to.throw("fecha mal o provincia vacia");
         });
 
-        it('3.4 solo 1 valor vacio', function() {
-            expect( pandemiun.updateDatos.bind("","Granada","contagiados") ).to.throw("nick, provincia o estado sin rellenar");
+        it('1.2.2 con provincia bien y fecha mal', function() {
+
+        expect( pandemiun.getDatos.bind("Granada","2020-10-") ).to.throw("fecha mal o provincia vacia");
         });
 
     });
+
+    //Test a API
+    /*context('2. Peticion GET a API ', function() {
+        it('peticion get sobre datos correctos /Granada/08-10-2020', (done) => {
+        chai.request(url)
+            .get('/Granada/2020-10-12')
+            .end( function(err,res){
+
+            done();
+        });
+
+    });*/
 
 });
