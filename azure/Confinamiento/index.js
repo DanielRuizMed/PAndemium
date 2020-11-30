1ef706d24a34b3825e3393fffd87f99a1a540196
DanielRuizMed <drm19933@gmail.com>
2020-11-30 19:43:45 +0100

    moviendo index a carpeta Confinamiento para que la funcion se llame asi en azure funcstions confinamiento() #94

---
+++ azure/Confinamiento/index.js
@@ -0,0 +1,24 @@
+
+const datos = require("../data/confinamiento.json")
+let estado = 400 
+
+//funcion handler
+module.exports = function handler(req, res) {
+
+	if( Object.keys(req.query).length == 1 ){
+		
+		result = datos.filter(it => it.ccaa === req.query.ccaa )[0]
+		estado = 200
+
+		if ( result == undefined ){
+			result = { "ccaa" : "Esa comunidad autonoma no existe"}
+			estado = 404
+		}
+
+	}else{
+		result = { "ccaa" : "No has indicado el parametro o no es el formato indicado: /api?ccaa=Andalucia" }
+	}
+
+	res.setHeader('Content-type','application/json');
+	res.status(estado).json(result)
+}
