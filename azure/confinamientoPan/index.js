03884555b65fe6b3863b7c6b9cf526a5780519d8
DanielRuizMed <drm19933@gmail.com>
2020-11-30 20:30:02 +0100

    arreglando errata confinamiento() de azure functions #94, #84

--- azure/confinamientoPan/index.js
+++ azure/confinamientoPan/index.js
@@ -3,22 +3,21 @@ const datos = require("./confinamiento.json")
 let estado = 400 
 
 //funcion handler
-module.exports = function handler(req, res) {
+module.exports = function handler(context, req) {
 
-	if( Object.keys(req.query).length == 1 ){
-		
-		result = datos.filter(it => it.ccaa === req.query.ccaa )[0]
-		estado = 200
+    if( Object.keys(req.query).length == 1 ){
+        
+        result = datos[req.query.ccaa]
+        estado = 200
 
-		if ( result == undefined ){
-			result = { "ccaa" : "Esa comunidad autonoma no existe"}
-			estado = 404
-		}
+        if ( result == undefined ){
+            result = { "ccaa" : "Esa comunidad autonoma no existe"}
+            estado = 404
+        }
+    }else{
+        result = { "ccaa" : "No has indicado el parametro o no es el formato indicado: /api?ccaa=Andalucia" }
+    }
 
-	}else{
-		result = { "ccaa" : "No has indicado el parametro o no es el formato indicado: /api?ccaa=Andalucia" }
-	}
-
-	res.setHeader('Content-type','application/json');
-	res.status(estado).json(result)
+	context.res.setHeader('Content-type','application/json');
+	context.res.status(estado).json(result)
 }
