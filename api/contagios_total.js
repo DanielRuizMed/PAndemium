e6d39968abb30ed1fe52d0cbd91ab00700957b94
DanielRuizMed <drm19933@gmail.com>
2020-11-30 13:02:22 +0100

    creando funcion contagios_total para desplegar en vercel #83 , #93

---
+++ api/contagios_total.js
@@ -0,0 +1,23 @@
+
+const datos = require("../src/json/contagios_total.json")
+let estado = 400
+
+//funcion handler
+module.exports = function handler(req, res) {
+
+	if( Object.keys(req.query).length == 1 ){
+		
+		result = datos.filter(it => it.ccaa === req.query.ccaa )[0]
+		estado = 200
+
+		if ( result == undefined ){
+			result = "Esa comunidad autonoma no existe";
+			estado = 404
+		}
+
+	}else{
+		result = "No has indicado el parametro o no es el formato indicado: /api?ccaa=Andalucia"
+	}
+
+	res.status(estado).json(result).setHeader("application/json");
+}
