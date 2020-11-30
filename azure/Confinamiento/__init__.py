0a4280351d40ce6aebcc724f9479f32e1a1ad51b
DanielRuizMed <drm19933@gmail.com>
2020-11-30 19:02:36 +0100

    restableciendo a versión inicial confinamiento() #94, #84

--- azure/Confinamiento/__init__.py
+++ azure/Confinamiento/__init__.py
@@ -1,11 +1,13 @@
 import logging
 import json, sys
 
-import azure.functions as func 
+import azure.functions as func
 
 # json con valores
-with open('./confinamiento.json') as f:
-    datos = json.loads(f)
+x = '{ "Andalucia":"confinada","Murcia":"confinada","Sevilla":"confinada","Canarias":"no confinada"}'
+
+# transformamos en json
+datos = json.loads(x)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
@@ -18,7 +20,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     else:
         result['result'] = "Nos has puesto bien los atributos, ejemplo: ?ccaa=Andalucia."
-        codigo = 400
+        codigo = 404
 
     return func.HttpResponse(
         json.dumps(result),
