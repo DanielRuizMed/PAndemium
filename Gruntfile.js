1f4592e09e246430bda902ac4e2e89eb69319f8d
DanielRuizMed <drm19933@gmail.com>
2020-12-08 11:56:53 +0100

    actualizando Gruntfile con las ordenes install y test utilizando para ellos el modulo grunt-shell para hacer llamadas a la terminal close #97

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,26 @@ module.exports = function(grunt) {
 
      // Add the grunt-mocha-test tasks.
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-shell');
  
   grunt.initConfig({
     // Configure a mochaTest task
     mochaTest: {
         src: ['test/**/*.js']
+    },
+    shell:{
+      install: {
+        command: 'npm install'
+      },
+      start:{
+        command: 'node src/index.js'
+      }
+      
     }
     
   });
  
   grunt.registerTask('test', 'mochaTest');
+  grunt.registerTask('install', ['shell:install']);
+  grunt.registerTask('start', ['shell:start']);
 }
